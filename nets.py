85684e9dd7fdff8ec96337023f1f73f1b6017e07
Ramapr <arcabuceroma79@gmail.com>
2022-02-02 18:06:30 +0300

    add models architectures

--- nets.py
+++ nets.py
@@ -38,3 +38,366 @@ def f1_m(y_true, y_pred):
   return 2*((precision*recall)/(precision+recall+K.epsilon()))
 #####
 
+def net_gap(input_shape):
+  inp = Input(input_shape)
+######## 1 
+  y1 = SeparableConv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  #y1 = Conv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  y1 = BatchNormalization()(y1)
+  #y1 = LeakyReLU()(y1)
+
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 2
+  y1 = SeparableConv2D(48, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = SeparableConv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 3
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = SeparableConv2D(16, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+####### 4 
+  y1 = SeparableConv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = GlobalAveragePooling2D()(y1)
+
+#######  
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(y1)
+  mdl = Model(inp, out)
+  return mdl
+
+def net_flat(input_shape):
+  inp = Input(input_shape)
+######## 1 
+  y1 = SeparableConv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  #y1 = Conv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  y1 = BatchNormalization()(y1)
+  #y1 = LeakyReLU()(y1)
+
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 2
+  y1 = SeparableConv2D(48, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = SeparableConv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 3
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = SeparableConv2D(16, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+####### 4 
+  y1 = SeparableConv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  #y1 = GlobalAveragePooling2D()(y1)
+  y1 = Flatten()(y1)
+  y1 = Dropout(0.3)(y1)
+#######  
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(y1)
+  mdl = Model(inp, out)
+  return mdl
+
+#%%
+
+
+
+def net_gap_conv(input_shape):
+  inp = Input(input_shape)
+######## 1 
+  y1 = Conv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  y1 = BatchNormalization()(y1)
+  
+  y1 = Conv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 2
+  y1 = Conv2D(48, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = Conv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 3
+  y1 = Conv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = Conv2D(16, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+####### 4 
+  y1 = Conv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = GlobalAveragePooling2D()(y1)
+#######  
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(y1)
+  mdl = Model(inp, out)
+  return mdl
+
+def net_flat_conv(input_shape):
+  inp = Input(input_shape)
+######## 1 
+  y1 = Conv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  y1 = BatchNormalization()(y1)
+
+  y1 = Conv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 2
+  y1 = Conv2D(48, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = Conv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 3
+  y1 = Conv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = Conv2D(16, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+####### 4 
+  y1 = Conv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  #y1 = GlobalAveragePooling2D()(y1)
+  y1 = Flatten()(y1)
+  y1 = Dropout(0.3)(y1)
+#######  
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(y1)
+  mdl = Model(inp, out)
+  return mdl
+
+#%%
+
+def net_gap_test(input_shape):
+  inp = Input(input_shape)
+######## 1 
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(inp)
+  y1 = BatchNormalization()(y1)
+
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 2
+  y1 = SeparableConv2D(64, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = SeparableConv2D(64, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 3
+  y1 = SeparableConv2D(128, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = SeparableConv2D(128, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+####### 4 
+  y1 = SeparableConv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = GlobalAveragePooling2D()(y1)
+#######  
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(y1)
+  mdl = Model(inp, out)
+  return mdl
+
+#%%
+def net_gap_strd(input_shape):
+  inp = Input(input_shape)
+######## 1 
+  y1 = SeparableConv2D(64, (3, 3), strides=(2, 2), padding='valid', activation='relu')(inp)
+  #y1 = Conv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  #y1 = SeparableConv2D(32, (3, 3), strides=(2, 2), padding='valid', activation='relu')(inp)
+  y1 = BatchNormalization()(y1)
+  #y1 = LeakyReLU()(y1)
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+####### 2
+  y1 = SeparableConv2D(48, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = SeparableConv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+####### 3
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = SeparableConv2D(16, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+####### 4 
+  y1 = SeparableConv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = GlobalAveragePooling2D()(y1)
+#######  
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(y1)
+  mdl = Model(inp, out)
+  return mdl
+
+
+
+
+#%%
+def net_flat_elu(inp_shape):
+  #inp_shape = (192, 256, 3)
+  inputs = Input(inp_shape)
+##### block 1
+  y1 = Conv2D(64, (3, 3), padding='valid', bias=False)(inputs)
+  y1 = BatchNormalization()(y1)
+  y1 = elu(y1)
+  #print(y1.shape)
+
+  y1 = Conv2D(32, (3, 3), padding='valid', bias=False)(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = elu(y1)
+  #print(y1.shape)
+
+  y1 = MaxPooling2D((2, 2))(y1)
+
+##### block 2
+  y1 = Conv2D(48, (3, 3), padding='valid', bias=False)(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = elu(y1)
+
+  y1 = Conv2D(24, (3, 3), padding='valid', bias=False)(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = elu(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+##### block 3
+  y1 = Conv2D(16, (3, 3), padding='valid', bias=False)(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = elu(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+  y1 = Conv2D(16, (1, 1), padding='valid', activation='relu')(y1)
+  x = Flatten()(y1)
+  x = Dropout(0.3)(x)
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(x)
+  mdl = Model(inputs, out)
+  return mdl
+
+#%%  
+def net_v3(inp_shape):
+  inputs = Input(inp_shape)
+  y1 = Conv2D(64, (3, 3), padding='valid', activation=elu)(inputs)
+  y1 = BatchNormalization()(y1)
+
+  y1 = Conv2D(32, (3, 3), padding='valid', activation=elu)(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = MaxPooling2D((2, 2))(y1)
+
+  y1 = Conv2D(48, (3, 3), padding='valid', activation=elu)(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = Conv2D(24, (3, 3), padding='valid', activation=elu)(y1)
+  y1 = BatchNormalization()(y1)
+
+  y1 = MaxPooling2D((2, 2))(y1)
+
+  y1 = Conv2D(16, (3, 3), padding='valid', activation=elu)(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+
+  y1 = Conv2D(16, (1, 1), padding='valid', activation=elu)(y1)
+
+  x = Flatten()(y1)
+  x = Dropout(0.3)(x)
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(x)
+  mdl = Model(inputs, out)
+  return mdl
+
+
+def net_t(input_shape):
+  inp = Input(input_shape)
+######## 1 
+  y1 = SeparableConv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  #y1 = Conv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  y1 = BatchNormalization()(y1)
+  #y1 = LeakyReLU()(y1)
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 2
+  y1 = SeparableConv2D(48, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = SeparableConv2D(24, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+
+####### 3
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+  #y1 = MaxPooling2D((2, 2))(y1)
+  y1 = SeparableConv2D(16, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = GlobalAveragePooling2D()(y1)
+#######  
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(y1)
+  mdl = Model(inp, out)
+  return mdl #decoded
+
+#%%
+  
+def net_r(input_shape):
+#  input_shape = (192, 256, 3)
+  inp = Input(input_shape)
+######## 1 
+  #y1 = SeparableConv2D(64, (3, 3), padding='valid', activation='relu')(inp)
+  y1 = Conv2D(16, (3, 3), padding='valid', activation='relu')(inp)
+  y1 = BatchNormalization()(y1)
+#  print('conv1', y1.shape)  
+  # here 1  
+  y1 = SeparableConv2D(16, (3, 3), padding='valid', activation='relu')(y1)
+#  print('conv02', y1.shape)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+#  print('conv2', y1.shape)  
+  x1 = Conv2D(1, (5, 5), padding='valid', strides=2, activation='relu')(y1)
+  x2 = Conv2D(32, (1, 1), padding='valid', activation='relu')(x1)
+#  x2.shape  
+####### 2
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+  y1 = BatchNormalization()(y1)
+#  print('conv3', y1.shape)
+  y1 = SeparableConv2D(32, (3, 3), padding='valid', activation='relu')(y1)
+#  print('conv04', y1.shape)
+  y1 = BatchNormalization()(y1)
+  y1 = MaxPooling2D((2, 2))(y1)
+#  print('conv4', y1.shape)
+  ad = Add()([x2, y1])
+####### 3
+  y1 = SeparableConv2D(64, (3, 3), padding='valid', activation='relu')(ad)
+  y1 = BatchNormalization()(y1)
+#  print('conv5', y1.shape)
+  #y1 = MaxPooling2D((2, 2))(y1)
+  y1 = SeparableConv2D(64, (3, 3), padding='valid', activation='relu')(y1)
+#  print('conv06', y1.shape)
+  y1 = BatchNormalization()(y1)
+  
+  y1 = GlobalAveragePooling2D()(y1)
+#######  
+  out = Dense(4, kernel_initializer='normal', activation='softmax')(y1)
+  mdl = Model(inp, out)
+  return mdl #decoded
+
+
+
